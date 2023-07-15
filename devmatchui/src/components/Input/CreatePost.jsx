1e92f38288a6c232cfcffc06edef6b83364577d9
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2023-07-15 01:27:47 -0700

    create post ui

--- devmatchui/src/components/Input/CreatePost.jsx
+++ devmatchui/src/components/Input/CreatePost.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const CreatePost = () => {
     console.log("Sfsfs")
   return (
-    <div className='absolute top-[50%] translate-y-[-50%] left-0 w-full p-4 bg-black border-2 rounded-md z-50 '>
+    <div className='fixed bottom-24  right-8 w-[90%] max-w-[300px] p-4 bg-black border-2 rounded-md z-50 '>
+        <h1 className='text-white text-lg font-semibold mb-2' >Create Post</h1>
         <textarea className='w-full bg-neutral-900 rounded-md p-2 text-neutral-300'></textarea>
+        <button className='w-full bg-blue-600 text-white font-semibold mt-2 p-2 rounded-md' >Post</button>
     </div>
   )
 }
